2ada7e16499966884561b34452b0fc81af7b67e3
Olishevko <54683421+Olishevko@users.noreply.github.com>
2019-09-29 17:53:13 +0300

    Add files via upload
    add week_of_day

---
+++ day_of_week.py
@@ -0,0 +1,18 @@
+
+day_of_week = int(input('enter the serial number of the day of the week: '))
+if day_of_week == 1:
+	print('monday')
+elif day_of_week == 2:
+	print('tuesday')
+elif day_of_week == 3:
+	print('wednesday')
+elif day_of_week == 4:
+	print('thursday')
+elif day_of_week == 5:
+	print('friday')
+elif day_of_week == 6:
+	print('saturday')
+elif day_of_week == 7:
+	print(sunday)
+else:
+	print('there is no such day of the week')
